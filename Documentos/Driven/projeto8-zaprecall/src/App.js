f53c44234d3cd40c64adb26825ad0593ef97369f
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-05-12 09:56:35 -0300

    telainicial

---
+++ Documentos/Driven/projeto8-zaprecall/src/App.js
@@ -0,0 +1,14 @@
+import Init from "./components/Init"
+import react from "react"
+import TelaFlashcards from "./components/TelaFlashcards"
+
+export default function App(){
+    const [tela, setTela] = react.useState("telaInicial")
+    return(
+        <div>
+            {tela === 'telaInicial' ? <Init setTela={setTela} />: <TelaFlashcards />
+            }
+        </div>
+    )
+
+}
---
+++ Documentos/Driven/projeto8-zaprecall/src/components/TelaFlashcards.js
@@ -0,0 +1,10 @@
+export default function TelaFlashcards(){
+    return(
+        <>
+          <header>
+              <img src=".logo" alt="" />
+              <h1>ZapRecall</h1>
+          </header>
+        </>
+    )
+}
