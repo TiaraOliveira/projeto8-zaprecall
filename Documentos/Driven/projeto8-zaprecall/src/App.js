0469e9757d5dace5313bdfeaa6e467df013c6a01
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-05-16 12:32:47 -0300

    finalizado com importação Css

--- Documentos/Driven/projeto8-zaprecall/src/App.js
+++ Documentos/Driven/projeto8-zaprecall/src/App.js
@@ -1,7 +1,8 @@
 import Init from "./components/Init"
 import react from "react"
 import TelaFlashcards from "./components/TelaFlashcards"
-
+import './components/style.css'
+import './components/reset.css'
 export default function App(){
     const [tela, setTela] = react.useState("telaInicial")
 
